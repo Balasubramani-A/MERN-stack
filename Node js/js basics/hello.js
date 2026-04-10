1224c5e68ac2e2eaf83e72c9fd9f42a6b77f6d23
Balasubramani A <balasubramani23bs@gmail.com>
2026-04-11 01:57:09 +0530

    new object added

--- Node js/js basics/hello.js
+++ Node js/js basics/hello.js
@@ -2,3 +2,8 @@
 let balance = 1000;
 console.log(balance);
 console.log(typeof (balance));
+
+let anotherBalance = new Number(2000);
+console.log(anotherBalance);
+console.log(typeof (anotherBalance));
+//[Number: 2000] -> Prints this because it is an object and not a primitive value
