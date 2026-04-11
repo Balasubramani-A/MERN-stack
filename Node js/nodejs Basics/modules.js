d5a79a1b7e8ef4e1b1d84d9770a71d8178d83684
Balasubramani A <balasubramani23bs@gmail.com>
2026-04-12 01:10:23 +0530

    mkdir, rmdir, unlink added

--- Node js/nodejs Basics/modules.js
+++ Node js/nodejs Basics/modules.js
@@ -6,15 +6,20 @@
 
 // 1. Built in modules
 fs = require('fs');
-console.log(fs);
+// console.log(fs);
 
 
 const content = fs.readFileSync('notes.txt', 'utf-8');
 //sync means it is a blocking operation 
-console.log(content);
+// console.log(content);
 
 
 fs.writeFileSync('copy.txt', 'This is a new note', 'utf-8');
 
 fs.writeFileSync('copy.txt', 'This is an updated note', 'utf-8'); //overwrites the content of the file
 fs.appendFileSync('copy.txt', '\nThis is an appended note', 'utf-8'); //appends the content to the file 
+
+fs.mkdirSync('games/xyz', {recursive: true}); //creates a directory and its parent directories if they do not exist
+fs.rmdirSync('games/xyz'); //removes the directory
+
+fs.unlinkSync('copy.txt'); //removes the file
