ae42434d2473da600ebed27f24e9f63a74b6329f
Balasubramani A <balasubramani23bs@gmail.com>
2026-04-13 02:02:46 +0530

    Register a one-time event listener

--- Node js/nodejs Basics/myEvents.js
+++ Node js/nodejs Basics/myEvents.js
@@ -4,6 +4,16 @@ const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
 
 // Register an event listener for the 'greet' event
-eventEmitter.on('greet', () => {
-    console.log('Hello, welcome to Node.js events!');
+eventEmitter.on('greet', (username) => {
+    console.log(`Hello, ${username} welcome to Node.js events!`);
 })
+
+// Register a one-time event listener for the 'pushnotification' event
+eventEmitter.once('pushnotification', (message) => {
+    console.log(`You have a new push notification: ${message}`);
+}); 
+
+//emit the 'greet' event
+eventEmitter.emit('greet', 'Balasubramani');
+
+eventEmitter.emit('pushnotification', 'You have a new message from Balu!');
