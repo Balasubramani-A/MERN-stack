12510c5587703ca2f9bbd7c9630fd2ad3ec0b081
Balasubramani A <balasubramani23bs@gmail.com>
2026-04-13 01:33:40 +0530

    add event - on

--- Node js/nodejs Basics/myEvents.js
+++ Node js/nodejs Basics/myEvents.js
@@ -1,3 +1,9 @@
 const EventEmitter = require('events');
 
+// Create an instance of EventEmitter
 const eventEmitter = new EventEmitter();
+
+// Register an event listener for the 'greet' event
+eventEmitter.on('greet', () => {
+    console.log('Hello, welcome to Node.js events!');
+})
