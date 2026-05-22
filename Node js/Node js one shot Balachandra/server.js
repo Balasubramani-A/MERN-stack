8f931618553bdd68926433b355581c289723a497
Balasubramani A <balasubramani23bs@gmail.com>
2026-05-22 22:32:52 +0530

    math functions and requirements added

--- Node js/Node js one shot Balachandra/server.js
+++ Node js/Node js one shot Balachandra/server.js
@@ -1,2 +1,16 @@
 console.log("Hello World");
-console.log(global);
+// console.log(global);
+
+// const os = require("os");
+// const path = require("path");
+const math = require("./math");
+
+console.log(math.add(2, 3));
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.dirname(__filename));
