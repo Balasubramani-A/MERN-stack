bc34775c0fff9765c743757555f9fccd0e0e3241
Balasubramani A <balasubramani23bs@gmail.com>
2026-05-22 22:58:13 +0530

    file system readfile added

--- Node js/Node js one shot Balachandra/server.js
+++ Node js/Node js one shot Balachandra/server.js
@@ -1,11 +1,12 @@
-console.log("Hello World");
+// console.log("Hello World");
 // console.log(global);
 
 // const os = require("os");
 // const path = require("path");
-const math = require("./math");
+// const math = require("./math");
 
-console.log(math.add(2, 3));
+
+
 // console.log(os.type());
 // console.log(os.version());
 // console.log(os.homedir());
@@ -14,3 +15,19 @@ console.log(math.add(2, 3));
 // console.log(__filename);
 
 // console.log(path.dirname(__filename));
+
+//arithmetic operations
+// console.log(math.add(2, 3));
+// console.log(math.subtract(5, 2));
+// console.log(math.multiply(4, 6));
+// console.log(math.divide(10, 2));
+
+const fs = require("fs");
+
+fs.readFile("./files/start.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  console.log("File contents:", data);
+});
