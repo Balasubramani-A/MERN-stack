e24f3d99ca107eb25174fb9138b89081e07392c0
Balasubramani A <balasubramani23bs@gmail.com>
2026-07-01 00:20:18 +0530

    middlewares add

--- Express js/web_server.js
+++ Express js/web_server.js
@@ -2,15 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
+
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+}
+
 //Middle wares
+//Used for fetching data from the client side, such as form data or JSON data. It is used to parse the incoming request body and make it available in the req.body object.
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 //built in middle wares
 //Error in the code below, the static files are not being served correctly. The issue is that the path to the static files is incorrect. The correct path should be 'public' instead of 'views'.
-console.log("Express is looking for static files in:", path.join(__dirname, 'public'));
-app.use(express.static(path.join(__dirname, 'public')));
+console.log("Express is looking for static files in:", path.join(__dirname, '/public'));
+// app.use(express.static(path.join(__dirname, 'public')));
+// Add '/public' as the first argument, and use path.resolve
+app.use('/public', express.static(path.resolve(__dirname, 'public')));
 
-
+// TEMPORARY DIAGNOSTIC ROUTE
+app.get('/test-css', (req, res) => {
+    const cssPath = path.join(__dirname, 'public', 'css', 'style.css');
+    console.log("Checking physical path:", cssPath);
+    res.sendFile(cssPath);
+});
 
 app.get(/^\/$|\/index(.html)?$/, (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -61,8 +75,6 @@ app.all(/.*/, (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-
-    
-
+});
 
     
