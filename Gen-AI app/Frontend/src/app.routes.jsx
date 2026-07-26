5139160e15720be888593c742f7dd01cd2b6c6b3
Balasubramani A <balasubramani23bs@gmail.com>
2026-07-26 16:37:28 +0530

    feat: register and login pages frontend add

--- Gen-AI app/Frontend/src/app.routes.jsx
+++ Gen-AI app/Frontend/src/app.routes.jsx
@@ -1,8 +1,19 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import Login from "./features/auth/pages/Login";
+import Register from "./features/auth/pages/Register";
+import App from "./App";
 
 export const router = createBrowserRouter([
+    
+  // {
+  //   path: "/",
+  //   element: <App />, // <-- Renders App component at the root URL "/"
+  // }, 
   {
-    path: "/",
-    element: <App />,
-  },
+    path: "/login",
+    element: <Login />,
+  },{
+    path: "/register",
+    element: <Register />,
+  }
 ]);
---
+++ Gen-AI app/Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useNavigate, Link } from "react-router";
+const Register = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+  }
+
+  return (
+    <main>
+        <div className="form-container">
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="input-group">
+                  <label htmlFor="username">Username:</label>
+                  <input type="text" id="username" name="username" placeholder="Enter Username" required />
+              </div>
+                <div className="input-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="Enter Email address" required />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button className="button primary-button" type="submit">Register</button>
+            </form>
+
+            <p>Already have an account? <Link to="/login">Login</Link></p>
+        </div>
+    </main>
+  );
+}   
+
+
+export default Register;
